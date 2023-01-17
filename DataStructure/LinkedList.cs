fc652385b68479cceaf9965d0e1cfa944377db2e
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-17 20:49:48 +0530

    Creating a simple Linked List

---
+++ DataStructure/LinkedList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class LinkedList
+    {
+        internal Node head;
+
+        public void Add(int data)
+        {
+            Node objNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = objNode;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = objNode;
+            }
+            Console.WriteLine($"\n{objNode.data} inserted into Linked List");
+        }
+
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("\nLinkedList is empty");
+                return;
+            }
+            Console.Write("\nLinked List => ");
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ DataStructure/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList");
+
+            LinkedList objLinkedList = new LinkedList();
+            objLinkedList.Add(56);
+            objLinkedList.Add(30);
+            objLinkedList.Add(70);
+            objLinkedList.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
